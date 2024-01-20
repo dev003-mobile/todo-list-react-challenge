5afa6fd7f80f087c86eaa9c6643546de7a5c932e
Wagner Coche <wagnercoche@gmail.com>
2024-01-20 23:34:31 +0100

    🐳 chore: custom add task textinput

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
 import styles from "./Header.module.css"
 import logoImage from "../assets/logo.svg"
+import plusImage from "../assets/plus.svg"
 
 export function Header() {
   return (
     <div className={styles.header}>
       <img className={styles.image} src={logoImage} />
+      <div className={styles.containerInput}>
+        <textarea
+          className={styles.input}
+          placeholder="Adicione uma nova tarefa" 
+        />
+        <button className={styles.button}>
+          Criar
+          <img src={plusImage} />
+        </button>
+      </div>
     </div>
   )
 }
